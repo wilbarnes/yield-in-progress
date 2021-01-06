f50f990719e7514ad9351bc27150f5cacd8c7647
wilbarnes <wbarnesf@gmail.com>
2021-01-06 16:54:42 -0500

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,75 @@
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+use solana_program::{msg, program_error::ProgramError, pubkey::Pubkey};
+use serum_pool::schema::{
+    declare_tag, AssetInfo, Basket, PoolState, FEE_RATE_DENOMINATOR, MIN_FEE_RATE, Address,
 };
+use serum_pool::{declare_pool_entrypoint, Pool, PoolContext};
+use borsh::{BorshDeserialize, BorshSerialize, BorshSchema};
 
-entrypoint!(process_instruction);
-fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
-    Ok(())
+declare_tag!(YieldInstructionTag, u64, 0x31e6452361a17878);
+
+#[derive(Clone, PartialEq, Eq, Debug, BorshSerialize, BorshDeserialize)]
+pub struct YieldInstruction {
+    tag: YieldInstructionTag,
+    inner: YieldInstructionInner,
 }
 
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        assert_matches::*,
-        solana_program::instruction::{AccountMeta, Instruction},
-        solana_program_test::*,
-        solana_sdk::{signature::Signer, transaction::Transaction},
-    };
+// NOTE: how to dynamically add new lending pools?
 
-    #[tokio::test]
-    async fn test_transaction() {
-        let program_id = Pubkey::new_unique();
+#[derive(Clone, PartialEq, Eq, Debug, BorshSerialize, BorshDeserialize)]
+pub enum YieldInstructionInner {
+    /// Rebalance between lenders
+    ///
+    /// Accounts:
+    ///
+	Rebalance,
+}
 
-        let (mut banks_client, payer, recent_blockhash) = ProgramTest::new(
-            "bpf_program_template",
-            program_id,
-            processor!(process_instruction),
-        )
-        .start()
-        .await;
+// NOTE: why do we need this?
+declare_tag!(YieldPoolTag, u64, 0x4a3ab7f76f93f94e);
 
-        let mut transaction = Transaction::new_with_payer(
-            &[Instruction {
-                program_id,
-                accounts: vec![AccountMeta::new(payer.pubkey(), false)],
-                data: vec![1, 2, 3],
-            }],
-            Some(&payer.pubkey()),
-        );
-        transaction.sign(&[&payer], recent_blockhash);
+#[derive(Clone, PartialEq, Eq, BorshSerialize, BorshDeserialize, Default)]
+pub struct CustomPoolState {
+    pub tag: YieldPoolTag,
+    pub yield_protocols: Vec<YieldProtocol>,
+}
 
-        assert_matches!(banks_client.process_transaction(transaction).await, Ok(()));
+trait CustomPoolStateContainer {
+    fn read_custom_state(&self) -> Result<CustomPoolState, ProgramError>;
+    fn write_custom_state(&mut self, custom_state: &CustomPoolState) -> Result<(), ProgramError>;
+}
+
+impl CustomPoolStateContainer for PoolState {
+    fn read_custom_state(&self) -> Result<CustomPoolState, ProgramError> {
+        CustomPoolState::try_from_slice(&self.custom_state).map_err(|_| {
+            msg!("Invalid pool custom state");
+            ProgramError::InvalidAccountData
+        })
+    }
+
+    fn write_custom_state(&mut self, custom_state: &CustomPoolState) -> Result<(), ProgramError> {
+        self.custom_state = custom_state.try_to_vec().unwrap();
+        Ok(())
     }
 }
+
+#[derive(Clone, PartialEq, Eq, BorshSerialize, BorshDeserialize)]
+pub struct YieldProtocol {
+    pub name: String,
+    pub yield_protocol_pubkey: Address,  // program id of lender
+}
+
+pub struct YieldPool;
+
+impl Pool for YieldPool {
+    fn initialize_pool(context: &PoolContext, state: &mut PoolState) -> Result<(), ProgramError> {
+        if context.custom_accounts.len() < 1 {
+            msg!("Missing yield protocol accounts.");
+            return Err(ProgramError::NotEnoughAccountKeys);
+        }
+        state.write_custom_state(&CustomPoolState::default())?;
+        Ok(())
+    }
+}
+
+#[cfg(not(feature = "no-entrypoint"))]
+declare_pool_entrypoint!(YieldPool);
